f4fcef89bbe6702495208f447e894e1c3ae9c283
osesantos <osesantos@proton.me>
2025-07-25 20:16:48 +0100

    project init

--- internal/transport/nats.go
+++ internal/transport/nats.go
@@ -1,9 +1,28 @@
 package transport
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+)
 
 type NatsClient struct {
 	conn *nats.Conn
+}
+
+type Subscription = nats.Subscription
+type Msg = nats.Msg
+type MsgHandler func(msg *Msg) error
+
+var ErrNatsConnectionNotEstablished = errors.New("Nats connection is not established")
+
+func NewNatsClient(natsURL string) (*NatsClient, error) {
+	natsClient, err := nats.Connect(natsURL)
+	if err != nil {
+		return nil, err
+	}
+	return &NatsClient{conn: natsClient}, nil
 }
 
 func (t *NatsClient) Publish(subject string, msg []byte) error {
@@ -13,9 +32,13 @@ func (t *NatsClient) Publish(subject string, msg []byte) error {
 	return t.conn.Publish(subject, msg)
 }
 
-func (t *NatsClient) Subscribe(subject string, cb nats.MsgHandler) error {
+func (t *NatsClient) Subscribe(subject string, handler MsgHandler) (*nats.Subscription, error) {
 	if t.conn == nil {
-		return fmt.Errorf("Nats connection is not established")
+		return nil, fmt.Errorf("Nats connection is not established")
 	}
-	return t.conn.Subscribe(subject, cb)
+	return t.conn.Subscribe(subject, func(msg *nats.Msg) {
+		if err := handler(msg); err != nil {
+			fmt.Printf("Error handling message: %v\n", err)
+		}
+	})
 }
--- synapse/client.go
+++ synapse/client.go
@@ -1,5 +1,53 @@
 package synapse
 
+import (
+	"encoding/json"
+
+	"github.com/osesantos/synapse/internal/transport"
+)
+
 type SynapseClient struct {
-	conn *nats.Conn
+	conn *transport.NatsClient
+}
+
+type SynapseMessageHandler func(msg AgentMessage) error
+
+func NewSynapseClient(natsURL string) (*SynapseClient, error) {
+	natsClient, err := transport.NewNatsClient(natsURL)
+	if err != nil {
+		return nil, err
+	}
+	return &SynapseClient{conn: natsClient}, nil
+}
+
+func (s *SynapseClient) Publish(subject string, msg AgentMessage) error {
+	if s.conn == nil {
+		return transport.ErrNatsConnectionNotEstablished
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	return s.conn.Publish(subject, data)
+}
+
+func (s *SynapseClient) Subscribe(subject string, handler SynapseMessageHandler) (*transport.Subscription, error) {
+	if s.conn == nil {
+		return nil, transport.ErrNatsConnectionNotEstablished
+	}
+
+	return s.conn.Subscribe(subject, func(msg *transport.Msg) error {
+		var agentMsg AgentMessage
+		if err := json.Unmarshal(msg.Data, &msg); err != nil {
+			return err
+		}
+
+		if err := handler(agentMsg); err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
